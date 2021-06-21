ba3fc5faa0064489276bce5327ab32fc12db8cdd
Anay Panshikar <48152950+anaypanshikar@users.noreply.github.com>
2021-06-21 13:39:53 +0530

    Add files via upload

--- Simulink/SMC/drone_parameters.m
+++ Simulink/SMC/drone_parameters.m
@@ -26,15 +26,33 @@ T = 50;
 Ts = 0.002; % Fixed Time step 
 
 %Desired position
-x_d = 20;   y_d = 65;   z_d = 50;   si_d = 1;
+x_d = 20;   y_d = 65;   z_d = 50;   si_d = 1*pi/180;
 
 % PID gain for Trajectory generation
 kp_d = 7;   kd_d = 8;   ki_d = 0.1;
 
+%PID gains for attitude controller
+kp_z = 7;   kd_z = 8;   ki_z = 0.5;
+kp_phi = 7;   kd_phi = 8;   ki_phi = 0.5;
+kp_theta = 7;   kd_theta = 8;   ki_theta = 0.5;
+kp_si = 7;   kd_si = 8;   ki_si = 0.5;
 
 % SMC constant 
 droneparam.lembda = 1;
-droneparam.zeta1 = -1 + 2*rand(1);
-droneparam.zeta2 = -1 + 2*rand(1);
-droneparam.zeta3 = -1 + 2*rand(1);
-droneparam.zeta4 = -1 + 2*rand(1);
+% droneparam.zeta1 = -1 + 2*rand(1);
+% droneparam.zeta2 = -1 + 2*rand(1);
+% droneparam.zeta3 = -1 + 2*rand(1);
+% droneparam.zeta4 = -1 + 2*rand(1);
+
+droneparam.zeta1 = 0;
+droneparam.zeta2 = 0;
+droneparam.zeta3 = 0;
+droneparam.zeta4 = 0;
+
+
+
+% Backstepping
+droneparam.c1 = 2; droneparam.c2 = 2;
+droneparam.c3 = 2; droneparam.c4 = 2;
+droneparam.c5 = 2; droneparam.c6 = 2; 
+droneparam.c7 = 2; droneparam.c8 = 2;
